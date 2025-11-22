b14d91c79ed6223badfd450afbd290f54a7c276f
AdityaPatel149 <55062974+adityapatel149@users.noreply.github.com>
2025-11-21 22:41:47 -0800

    Add sample file

---
+++ sb3_test3.py
@@ -0,0 +1,48 @@
+﻿import gymnasium as gym
+import highway_env
+from stable_baselines3 import DQN
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.monitor import Monitor
+import time
+
+
+def make_env():
+    def _init():
+        env = gym.make("highway-v0", disable_env_checker=True)
+        env.unwrapped.configure({
+            "observation": {"type": "Kinematics"},
+            "simulation_frequency": 10,
+            "policy_frequency": 2,
+            "duration": 40,
+            "vehicles_count": 20,
+        })
+        env = Monitor(env)  # Enables episode reward/length logging
+        return env
+    return _init
+
+if __name__ == "__main__":
+    # Only run multiprocessing code here
+    env = SubprocVecEnv([make_env() for _ in range(4)]) # can use 8 for more envs in parallel if you have more cores.
+
+    model = DQN(
+        "MlpPolicy",
+        env,
+        learning_rate=1e-5,
+        buffer_size=1000000,
+        batch_size=64,
+        #train_freq=4,
+        target_update_interval=3000,
+        verbose=1,
+        device="cpu",
+        tensorboard_log="dqn_tensorboard_log"
+    )
+
+    print("Starting DQN training...")
+    start = time.time()
+    model.learn(total_timesteps=100_000)
+    print(f"Done!")
+
+    model.save("dqn_highway_fast")
+    print("Model saved as dqn_highway_fast.zip")
+
+    env.close()
